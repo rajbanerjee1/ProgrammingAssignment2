1a174ec81b5017f94b8e2ec95b713b5a48b580d7
Raj Banerjee <raj01banerjee@gmail.com>
2020-04-28 15:26:43 +0530

    proposed solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 ## Creating a pair of functions that can cache the inverse of a matrix.
 
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
